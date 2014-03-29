545284ca9b55972e70c76dbd39c37e2f84b518c8
BBartosz <bbartek91@gmail.com>
2014-03-29 16:53:51 +0100

    little refactor

--- scanner.rb
+++ scanner.rb
@@ -1,21 +1,26 @@
 
-Token = Struct.new(:token, :word)
+Token = Struct.new(:token, :def)
 class Scanner
 
   
   def initialize (input)
     tokens = []
     current_token_number = 0
-    tokenize(input).each do |token_array|
-      token_array.each do |token|
-        puts token.match(tok_regex).inspect if token
-      end
-    end
+    tokenize(input)
   end
 
   def tokenize(input)
     string_to_tokenize = input.clone
-    string_to_tokenize.scan(tok_regex)
+    array_of_token_arrays = string_to_tokenize.scan(tok_regex)
+    array_of_token_arrays.each do |token_array|
+      token_array.each do |token|
+        if token
+          #tu poiwnienem zwrócić liste tokenół najlepiej wraz z
+          puts token.inspect
+        end
+      end
+    end
+
   end
 
   def tok_regex
@@ -26,18 +31,21 @@ class Scanner
     ["SCOLON" , /\;/],
     ["OCB" , /\{/],
     ["CCB" , /\}/],
-    ["SELECTOR" , /[.#]?[a-zA-Z]+[0-9]?/], 
-    ["WHITESPACE" , /\s+/]] 
+    ["SELECTOR" , /[.#]?[a-zA-Z]+[0-9]?/]]  
 
-    tok_regex = '' 
-    
+    token_group_str = create_token_group_string(token_specification)
 
-    token_specification.each do |pair|
-      tok_regex += ('(?<%s>%s)' % pair) 
-      tok_regex += '|' if pair != token_specification.last
+    token_group_regex = %r{#{token_group_str}}
+  end
+
+  def create_token_group_string(token_spec)
+    token_group_string = ''
+
+    token_spec.each do |pair|
+      token_group_string += ('(?<%s>%s)' % pair) 
+      token_group_string += '|' if pair != token_spec.last
     end
-    tok_regex = %r{#{tok_regex}}
-    tok_regex
+    token_group_string
   end
 end
 
